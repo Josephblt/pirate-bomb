8ac516b507f2b2027bd030863ac5983543b500e0
Wagner Scholl Lemos <josephblt@gmail.com>
2022-05-17 22:21:15 -0300

    Preparing for Mobile Support

--- Godot/Scripts/Characters/Player Spawn.gd
+++ Godot/Scripts/Characters/Player Spawn.gd
@@ -7,6 +7,8 @@ export var spawn_on_start = true
 onready var door = $"Door"
 onready var scene_root = $"/root"
 
+var player
+
 
 func _ready():
 	if spawn_on_start:
@@ -14,7 +16,13 @@ func _ready():
 
 
 func spawn():
-	var player = Player.instance()
+	player = Player.instance()
 	player.position = position
+	player.connect("tree_exiting", self, "_on_tree_exiting")
 	scene_root.call_deferred("add_child", player)
 	door.open()
+
+
+func _on_tree_exiting():
+	player.disconnect("tree_exiting", self, "_on_tree_exiting")
+	spawn()
--- Godot/Scripts/D-Pad Util.gd
+++ Godot/Scripts/D-Pad Util.gd
@@ -1,6 +1,9 @@
 extends Node
 
 
+var mobile_move = Vector2.ZERO
+
+
 func player_move_pressed():
 	var horizontal = player_move_horizontal_pressed() 
 	var down = player_move_down_pressed()
@@ -10,7 +13,8 @@ func player_move_pressed():
 func player_move_down_pressed():
 	var down = Input.is_action_pressed("Player Down")
 	var analog_down = Input.is_action_pressed("Player Down (Analog)")
-	return down or analog_down
+	var mobile_down = mobile_move.y >= 0.25
+	return down or analog_down or mobile_down
 
 
 func player_move_horizontal_pressed():
@@ -22,10 +26,12 @@ func player_move_horizontal_pressed():
 func player_move_left_pressed():
 	var left = Input.is_action_pressed("Player Left") 
 	var analog_left = Input.is_action_pressed("Player Left (Analog)")
-	return left or analog_left
+	var mobile_left = mobile_move.x <= -0.25
+	return left or analog_left or mobile_left
 
 
 func player_move_right_pressed():
 	var right = Input.is_action_pressed("Player Right")
 	var analog_right = Input.is_action_pressed("Player Right (Analog)")
-	return right or analog_right
+	var mobile_right = mobile_move.x >= 0.25
+	return right or analog_right or mobile_right
